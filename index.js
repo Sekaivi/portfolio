542fd436574b9a901f11a92a9cbb3fd48353f12b
Sekaivi <rouibahmayssa@gmail.com>
2025-04-15 23:07:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
